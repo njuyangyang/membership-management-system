170fb787c095a61e76d3685d7e5cda233e579894
catherinetian <whm660403@tamu.edu>
2016-04-20 01:43:35 +0000

    TJ_Step4

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get 'static_pages/privacy'
   get 'static_pages/terms'
   get 'static_pages/characters'
+  get 'static_pages/images'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
