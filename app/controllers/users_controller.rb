79e4d49858f1309fdf083a8519249394138f0b8f
catherinetian <whm660403@tamu.edu>
2016-05-05 14:36:38 +0000

    Final Version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -189,4 +189,24 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :uin, :tel, :email, :membership, :shirt, :classification, :dynasty)
     end
+    
+    def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
